2922c4bdd423705c20072e31a7ecc714b2101236
Felipe Junges <felipeejunges@hotmail.com>
2025-06-22 15:07:12 -0300

    Fix deploy

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ require 'sidekiq-scheduler/web'
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
   # Only mount Sidekiq web UI in development and test environments
   unless Rails.env.production?
     mount Sidekiq::Web => '/sidekiq'
